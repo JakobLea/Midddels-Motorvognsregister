ea95c80fdfdd81e408f13b32e01adb3b0a480133
JakobLea <jakob.lea@outlook.com>
2024-02-12 23:54:06 +0100

    Endret data.sql for at table skal starte tomt og endret på css

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,1 @@
-
-
-INSERT INTO Kunde (navn, adresse, persNr, bilskilt, bilMerke, bilModell) VALUES ( 'Per Hansen','Osloveien 82', '29938501982', 'DR72401', 'BMW', 'iX');
-INSERT INTO Kunde (navn, adresse, persNr, bilskilt, bilMerke, bilModell) VALUES ( 'Line Jensen','Askerveien 5', '31255183720', 'BS94792', 'Audi', 'R8');
+DELETE FROM Kunde; --Sletter alt av data når programet kjøres, så det kan starte med ett blankt table
